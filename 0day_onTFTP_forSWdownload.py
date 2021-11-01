ec80632b065b65bdcc23c0b03b1a971d24e84139
Joe <qiaolibo@126.com>
2021-11-01 08:39:05 +0800

    Update 0day_onTFTP_forSWdownload.py

--- 0day_onTFTP_forSWdownload.py
+++ 0day_onTFTP_forSWdownload.py
@@ -1,7 +1,7 @@
 print("\n\n *** Day0 Autostart Python Script *** \n\n")
 import cli
 print("\n\n *** username & vty *** \n\n")
-cli.configurep(["username wafer privilege 15 password Wafer!234", "enable password Wafer!234", "line vty 0 15", "login local", "transport input all", "end"])
+cli.configurep(["username username privilege 15 password password", "enable password password", "line vty 0 15", "login local", "transport input all", "end"])
 #print("\n\n *** image download *** \n\n")
 #print("downloading\n")
 #cli.command = "copy tftp://172.31.56.253/cat9k_iosxe.17.03.04.SPA.bin flash:\n"
